09baf61e4e30bfd2b2771263875e9aa986ff1796
Kaycee Coleman <kaycee.coleman@gmail.com>
2020-01-03 15:37:15 -0500

    Update Table1_subset_for_Carrie.R

--- finalReport/docksideMonitoring2019/Table1_subset_for_Carrie.R
+++ finalReport/docksideMonitoring2019/Table1_subset_for_Carrie.R
@@ -138,3 +138,128 @@ write.table(xTable,
             quote = FALSE,
             col.names = FALSE,
             row.names = FALSE)
+# -------------- #
+
+
+# -------------- #
+# FF
+# -------------- #
+WM_FF = WM %>% filter(Fishery %in% "Finfish")
+RM_FF = RM %>% filter(Fishery %in% "Finfish")
+
+tripSummary = as.data.frame(matrix(data = NA, ncol=7, nrow=7))
+names(tripSummary) = c("Regions","AvailTrips","AttemptedTrips","SuccessfulTrips","AvailWM","AttemptedWM","SuccessfulWM")
+tripSummary$Regions = c("1","2","3","4","5","6","Total")
+
+tripSummary[tripSummary$Regions %in% "Total",2:7] = c(prettyNum(length(unique(WM_FF$TripID)), big.mark = ","), 
+                                                      paste(formatC(length(unique(RM_FF$TripID))/length(unique(WM_FF$TripID))*100, digits = 3), "% (n = ", length(unique(RM_FF$TripID)), ")", sep=""),
+                                                      paste(formatC((length(unique(RM_FF$TripID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED")]))/length(unique(WM_FF$TripID)))*100, digits=3), 
+                                                            "% (n = ", length(unique(RM_FF$TripID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED")])), ")", sep=""),
+                                                      length(unique(WM_FF$DNRID)), 
+                                                      paste(formatC((length(unique(RM_FF$DNRID))/length(unique(WM_FF$DNRID)))*100, digits=4), 
+                                                            "% (n = ", length(unique(RM_FF$DNRID)), ")", sep=""),
+                                                      paste(formatC((length(unique(RM_FF$DNRID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED")]))/length(unique(WM_FF$DNRID)))*100, digits=4), 
+                                                            "% (n = ",length(unique(RM_FF$DNRID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED")])), ")", sep="")) 
+
+for(n in c(1:6)){
+  tripSummary$AvailTrips[n] = prettyNum(length(unique(WM_FF$TripID[WM_FF$region %in% n])), big.mark = ",")
+  tripSummary$AttemptedTrips[n] = paste(formatC(length(unique(RM_FF$TripID[RM_FF$region %in% n]))/length(unique(WM_FF$TripID[WM_FF$region %in% n]))*100, digits = 3), "% (n = ", length(unique(RM_FF$TripID[RM_FF$region %in% n])), ")", sep="")
+  tripSummary$SuccessfulTrips[n] = paste(formatC((length(unique(RM_FF$TripID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED") & RM_FF$region %in% n]))/length(unique(WM_FF$TripID[WM_FF$region %in% n])))*100, digits=3), 
+                                         "% (n = ", length(unique(RM_FF$TripID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED") & RM_FF$region %in% n])), ")", sep="")
+  tripSummary$AvailWM[n] = length(unique(WM_FF$DNRID[WM_FF$region %in% n]))
+  tripSummary$AttemptedWM[n] = paste(formatC((length(unique(RM_FF$DNRID[RM_FF$region %in% n]))/length(unique(WM_FF$DNRID[WM_FF$region %in% n])))*100, digits=4), 
+                                     "% (n = ", length(unique(RM_FF$DNRID[RM_FF$region %in% n])), ")", sep="")
+  tripSummary$SuccessfulWM[n] = paste(formatC((length(unique(RM_FF$DNRID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED") & RM_FF$region %in% n]))/length(unique(WM_FF$DNRID[WM_FF$region %in% n])))*100, digits=4), 
+                                      "% (n = ",length(unique(RM_FF$DNRID[RM_FF$Result %in% c("MONITORED (on paper)","MONITORED") & RM_FF$region %in% n])), ")", sep="") 
+}
+rm(n)
+
+xTable =  htmlTable(tripSummary, rnames = FALSE,
+                    caption="Table 2. Finfish Trip Summary for Roving Monitors July 1 to December 15, 2019",
+                    header =  c("Region",
+                                "Total Available Trips",	
+                                "Attempted Trips Monitored",	
+                                "Successful Trips Monitored",	
+                                "Number of Available Watermen",	
+                                "Number of Individual Watermen Attempted to be Monitored",
+                                "Number of Individual Watermen Successfully Monitored"),
+                    n.rgroup = c(6,1),
+                    align = "lc",
+                    align.header = "lccc",
+                    css.cell = rbind(rep("font-size: 1.1em; padding-right: 0.6em", 
+                                         times=7), matrix("font-size: 1.1em; padding-right: 0.6em", ncol=7, nrow=7)),
+                    css.table = "margin-top: 1em; margin-bottom: 1em; table-layout: fixed; width: 1000px",
+                    total = "tspanner",
+                    css.total = c("border-top: 1px solid grey; font-weight: 900"),
+                    n.tspanner = c(nrow(tripSummary)))
+xTable 
+
+write.table(xTable, 
+            file=paste(dir.out, "Table1FF_for_Carrie.html",sep=""), 
+            quote = FALSE,
+            col.names = FALSE,
+            row.names = FALSE)
+#
+# -------------- #
+
+
+# -------------- #
+# BC
+# -------------- #
+WM_BC = WM %>% filter(Fishery %in% "Blue Crab")
+RM_BC = RM %>% filter(Fishery %in% "Blue Crab")
+
+tripSummary = as.data.frame(matrix(data = NA, ncol=7, nrow=7))
+names(tripSummary) = c("Regions","AvailTrips","AttemptedTrips","SuccessfulTrips","AvailWM","AttemptedWM","SuccessfulWM")
+tripSummary$Regions = c("1","2","3","4","5","6","Total")
+
+tripSummary[tripSummary$Regions %in% "Total",2:7] = c(prettyNum(length(unique(WM_BC$TripID)), big.mark = ","), 
+                                                      paste(formatC(length(unique(RM_BC$TripID))/length(unique(WM_BC$TripID))*100, digits = 3), "% (n = ", length(unique(RM_BC$TripID)), ")", sep=""),
+                                                      paste(formatC((length(unique(RM_BC$TripID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED")]))/length(unique(WM_BC$TripID)))*100, digits=3), 
+                                                            "% (n = ", length(unique(RM_BC$TripID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED")])), ")", sep=""),
+                                                      length(unique(WM_BC$DNRID)), 
+                                                      paste(formatC((length(unique(RM_BC$DNRID))/length(unique(WM_BC$DNRID)))*100, digits=4), 
+                                                            "% (n = ", length(unique(RM_BC$DNRID)), ")", sep=""),
+                                                      paste(formatC((length(unique(RM_BC$DNRID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED")]))/length(unique(WM_BC$DNRID)))*100, digits=4), 
+                                                            "% (n = ",length(unique(RM_BC$DNRID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED")])), ")", sep="")) 
+
+for(n in c(1:6)){
+  tripSummary$AvailTrips[n] = prettyNum(length(unique(WM_BC$TripID[WM_BC$region %in% n])), big.mark = ",")
+  tripSummary$AttemptedTrips[n] = paste(formatC(length(unique(RM_BC$TripID[RM_BC$region %in% n]))/length(unique(WM_BC$TripID[WM_BC$region %in% n]))*100, digits = 3), "% (n = ", length(unique(RM_BC$TripID[RM_BC$region %in% n])), ")", sep="")
+  tripSummary$SuccessfulTrips[n] = paste(formatC((length(unique(RM_BC$TripID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED") & RM_BC$region %in% n]))/length(unique(WM_BC$TripID[WM_BC$region %in% n])))*100, digits=3), 
+                                         "% (n = ", length(unique(RM_BC$TripID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED") & RM_BC$region %in% n])), ")", sep="")
+  tripSummary$AvailWM[n] = length(unique(WM_BC$DNRID[WM_BC$region %in% n]))
+  tripSummary$AttemptedWM[n] = paste(formatC((length(unique(RM_BC$DNRID[RM_BC$region %in% n]))/length(unique(WM_BC$DNRID[WM_BC$region %in% n])))*100, digits=4), 
+                                     "% (n = ", length(unique(RM_BC$DNRID[RM_BC$region %in% n])), ")", sep="")
+  tripSummary$SuccessfulWM[n] = paste(formatC((length(unique(RM_BC$DNRID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED") & RM_BC$region %in% n]))/length(unique(WM_BC$DNRID[WM_BC$region %in% n])))*100, digits=4), 
+                                      "% (n = ",length(unique(RM_BC$DNRID[RM_BC$Result %in% c("MONITORED (on paper)","MONITORED") & RM_BC$region %in% n])), ")", sep="") 
+}
+rm(n)
+
+xTable =  htmlTable(tripSummary, rnames = FALSE,
+                    caption="Table 3. Blue Crab Trip Summary for Roving Monitors July 1 to December 15, 2019",
+                    header =  c("Region",
+                                "Total Available Trips",	
+                                "Attempted Trips Monitored",	
+                                "Successful Trips Monitored",	
+                                "Number of Available Watermen",	
+                                "Number of Individual Watermen Attempted to be Monitored",
+                                "Number of Individual Watermen Successfully Monitored"),
+                    n.rgroup = c(6,1),
+                    align = "lc",
+                    align.header = "lccc",
+                    css.cell = rbind(rep("font-size: 1.1em; padding-right: 0.6em", 
+                                         times=7), matrix("font-size: 1.1em; padding-right: 0.6em", ncol=7, nrow=7)),
+                    css.table = "margin-top: 1em; margin-bottom: 1em; table-layout: fixed; width: 1000px",
+                    total = "tspanner",
+                    css.total = c("border-top: 1px solid grey; font-weight: 900"),
+                    n.tspanner = c(nrow(tripSummary)))
+xTable 
+
+write.table(xTable, 
+            file=paste(dir.out, "Table1BC_for_Carrie.html",sep=""), 
+            quote = FALSE,
+            col.names = FALSE,
+            row.names = FALSE)
+#
+# -------------- #
