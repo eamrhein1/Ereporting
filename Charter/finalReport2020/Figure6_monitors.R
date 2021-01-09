1d7323b7dd7121859c49de00523ee2038d170f35
Kaycee Coleman <kaycee.coleman@gmail.com>
2021-01-08 19:36:03 -0500

    Update Figure6_monitors.R

--- Charter/finalReport2020/Figure6_monitors.R
+++ Charter/finalReport2020/Figure6_monitors.R
@@ -17,8 +17,8 @@ library(readxl)
 # -------------------- #
 # load data
 # -------------------- #
-trips <- read_csv("~/Oyster Recovery Partnership, Inc/ORP - Operations/Sustainable Fisheries/E-Reporting/Data/FACTSdata/rawdata/CharterTrips_122920.csv")
-reports <- read_csv("~/Oyster Recovery Partnership, Inc/ORP - Operations/Sustainable Fisheries/E-Reporting/Data/FACTSdata/rawdata/CharterMonitorReports_122920.csv")
+trips <- read_csv("~/Oyster Recovery Partnership, Inc/ORP - Operations/Sustainable Fisheries/E-Reporting/Data/FACTSdata/rawdata/CharterTrips_010720.csv")
+reports <- read_csv("~/Oyster Recovery Partnership, Inc/ORP - Operations/Sustainable Fisheries/E-Reporting/Data/FACTSdata/rawdata/CharterMonitorReports_010720.csv")
 timeslots <- read_excel("~/Oyster Recovery Partnership, Inc/ORP - Operations/Sustainable Fisheries/E-Reporting/Pilot Projects/Charter/Year 1/Final Report/RM shifts worked for analysis.xlsx")
 # -------------------- #
 
@@ -67,9 +67,17 @@ trips = trips %>%
 
 # how many monitoring trips were done
 length(unique(reports$TripID))
+length(unique(reports$TripID[reports$Onboard %in% "Y"]))
+length(unique(reports$TripID[reports$Onboard %in% "N"]))
 
 # how many successful monitoring trips were there
-length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper")]))
+length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper)")]))
+(length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper)")]))/ length(unique(reports$TripID))) * 100
+
+length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper)") & reports$Onboard %in% "Y"]))
+
+length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper)") & reports$Onboard %in% "N"]))
+(length(unique(reports$TripID[reports$Result %in% c("MONITORED", "MONITORED (on paper)") & reports$Onboard %in% "N"]))/ length(unique(reports$TripID[reports$Onboard %in% "N"]))) * 100
 # -------------------- #
 
 
@@ -113,7 +121,7 @@ spp_count_combo = inner_join(dplyr::select(trips, TripID, Species, Count, Dispos
          monitor_count = Count.y) %>%
   mutate(count_diff = trip_count - monitor_count)
 
-# percent of trips monitors missed the angler count
+# percent of trips monitors missed the species count
 (length(which(is.na(spp_count_combo$count_diff)==TRUE))/length(spp_count_combo$count_diff))*100
 # percent of trips monitors angler count was the same as captain angler count
 (length(which(spp_count_combo$count_diff==0))/length(spp_count_combo$count_diff))*100
@@ -141,28 +149,55 @@ p
 rm(spp_count_combo)
 
 ###### Species Weight
-spp_quantity_combo = inner_join(dplyr::select(trips, TripID, Species, Quantity, Disposition) %>% 
-                               distinct() %>%
-                               mutate(Disposition = replace(Disposition, Disposition %in% "Kept - fileted", "Kept")) %>%
-                               group_by(TripID, Species, Disposition) %>%
-                               summarise(Quantity = sum(Quantity)), 
+
+# how many fish were filleted before they reacted the dock (e.g. captain said kept, monitor said kept fileted)
+# this may not be right
+x = dplyr::select(reports, TripID, SpeciesGrade, Quantity, Disposition, Result) %>% 
+  rename(Species = SpeciesGrade) %>%
+  filter(Result %in% c("MONITORED","MONITORED (on paper)")) %>% 
+  distinct() %>% 
+  filter(!Disposition %in% "Released") 
+y = dplyr::select(trips, TripID, Species, Quantity, Disposition) %>%
+  distinct() %>%
+  filter(!Disposition %in% "Released",
+         TripID %in% x$TripID)
+not_matched = full_join(x,y,by=c("TripID","Species","Disposition")) %>%
+  mutate(q = sum(Quantity.x, Quantity.y, na.rm=FALSE)) %>%
+  filter(is.na(q)) %>%
+  group_by(TripID, Species) %>% 
+  summarise(n=n()) %>% 
+  filter(!n %in% 1)
+
+spp_disp_change = 
+  
+# combo 
+spp_quantity_combo = inner_join(dplyr::select(trips, TripID, Species, Quantity, Disposition) %>%
+                                  distinct() %>%
+                                  filter(Disposition %in% c("Kept")), 
                              dplyr::select(reports, TripID, SpeciesGrade, Quantity, Disposition, Result) %>% 
                                rename(Species = SpeciesGrade) %>%
                                filter(Result %in% c("MONITORED","MONITORED (on paper)")) %>% 
                                distinct() %>% 
-                               mutate(Disposition = replace(Disposition, Disposition %in% "Kept - fileted", "Kept")) %>%
-                               group_by(TripID, Species, Disposition) %>%
-                               summarise(Quantity = sum(Quantity)), 
+                               filter(Disposition %in% c("Kept")), 
                              by = c("TripID","Species","Disposition")) %>%
   rename(trip_quantity = Quantity.x,
          monitor_quantity = Quantity.y) %>%
-  mutate(quantity_diff = trip_quantity - monitor_quantity)
+  mutate(quantity_diff = trip_quantity - monitor_quantity) 
+
+# percent of reports that match
+(length(which(spp_quantity_combo$quantity_diff==0))/length(spp_quantity_combo$quantity_diff))*100
 
 # range weights were off by
 mean(spp_quantity_combo$quantity_diff, na.rm=T)
+sd(spp_quantity_combo$quantity_diff, na.rm=T)
+
 median(spp_quantity_combo$quantity_diff, na.rm=T)
 min(spp_quantity_combo$quantity_diff, na.rm=T)
 max(spp_quantity_combo$quantity_diff, na.rm=T)
+
+# visually inspect which species had counts off 
+# typically more bait fish had higher counts off
+x = filter(spp_quantity_combo, !quantity_diff %in% 0)
 
 # plot
 p = ggplot() + 
